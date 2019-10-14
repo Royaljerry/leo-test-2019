a4c62d8d92167fd48975d3a0a0765c45d0abbd29
Adam Pócs <adam.pocs@edreamsodigeo.com>
2019-10-14 18:43:49 +0200

    Open autok.txt and read its contents

---
+++ 4-ceges-autok/autok.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+autok_obj = open('autok.txt', 'r')
+autok_content = autok_obj.readlines()
+print(autok_content)
+autok_obj.close()
+
+
+
+# split_content = autok_content.split(' ')
+
