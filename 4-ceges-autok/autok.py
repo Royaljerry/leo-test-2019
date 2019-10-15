c987ba1799803cf91dd2180d0dcdc540d666ab99
Adam Pócs <adam.pocs@edreamsodigeo.com>
2019-10-15 18:21:13 +0200

    Add program plan

--- 4-ceges-autok/autok.py
+++ 4-ceges-autok/autok.py
@@ -6,13 +6,8 @@ autok_obj.close()
 
 for record in autok_content:
 	field = record.strip().split()[0]
-	# print(field)
-	print(record.strip())
+	print(field)
+	# print(record.strip())
 
-DAY = (field)
-
-print (DAY)
-
-
-# split_content = autok_content.split(' ')
-
+# iterálás az összes elemen (for…)
+# ha az adott record utolsó field-je = 0 => első field megjegyzése
