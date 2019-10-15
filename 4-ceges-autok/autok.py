1213fe537e73837777ff180389bae782490f3f5a
leocsi <horvath.leo2001@gmail.com>
2019-10-15 19:51:20 +0200

    Complete task 3

--- 4-ceges-autok/autok.py
+++ 4-ceges-autok/autok.py
@@ -35,3 +35,16 @@ for record in car_records:
 		car_status = car_statuses[int(fields[5])]
 		car_out_string = fields[1] + fields[2] + fields[4] + str(car_status)
 		print(car_out_string)
+
+# =========== Task #3 ===========
+
+cars_rented = []
+
+for record in car_records: 
+	fields = record.strip().split()
+
+	if (fields[5] == "0"): 
+		cars_rented.append(fields[2])
+	else: 
+		cars_rented.remove(fields[2])
+print ("A hónap végén " + str(len(cars_rented)) +" autót nem hoztak vissza.")
